eede1c50f25f0b4bcc4d5bd319f14229efba957d
hammadkh0 <hammad5718@gmail.com>
2023-06-13 23:12:27 +0500

    Feat: Added Toast Notification

--- src/utils/toastMessages.js
+++ src/utils/toastMessages.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 export const toastSuccess = (message) => {
   toast.success(message, {
     position: "top-center",
-    autoClose: 1500,
+    autoClose: 1000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: false,
@@ -16,7 +16,7 @@ export const toastSuccess = (message) => {
 export const toastError = (message) => {
   toast.error(message, {
     position: "top-center",
-    autoClose: 1500,
+    autoClose: 1000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: false,
